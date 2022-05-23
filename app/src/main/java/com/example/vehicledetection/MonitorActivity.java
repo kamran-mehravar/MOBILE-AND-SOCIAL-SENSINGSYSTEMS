207e7b130e095be73a3c0c0ee5a823dafa0e0b9f
bruno casu <brunocasu8@gmail.com>
2022-05-23 15:12:11 +0200

    monitoring data collection completed - merge with plot addition

--- app/src/main/java/com/example/vehicledetection/MonitorActivity.java
+++ app/src/main/java/com/example/vehicledetection/MonitorActivity.java
@@ -38,9 +38,13 @@ import java.io.FileWriter;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Map;
+
 
 //import com.beust.jcommander.JCommander;
 //import com.beust.jcommander.Parameter;
@@ -53,8 +57,9 @@ import java.util.Map;
 
 public class MonitorActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
 
-    private static final int MONITORING_REPETITIONS = 6; // collect data for 6 time windows (5s each)
-    private static final int RECORDS_SEC = 50;
+    private static final int MONITORING_REPETITIONS = 2; // collect data for 6 time windows (5s each)
+    private static final int SAMPLE_SIZE = 250;
+    private static final int RECORDS_SEC = 40;
     private static final int BUS = 0;
     private static final int CAR = 1;
     private static final int MOTO = 2;
@@ -67,12 +72,9 @@ public class MonitorActivity extends AppCompatActivity implements SensorEventLis
     private Context c;
     private File inferenceTempFile, filteredDataFile;
     private StringBuilder rawData, filteredData;
-    private DataWindow window;
     private XYPlot plot;
 
-    // accelerometer data
-    private final int SAMPLE_SIZE = RECORDS_SEC * 5; // 5 seconds of window size
-    private final float[] rawAccData = new float[3];
+    private final float[] rawAccData = new float[SAMPLE_SIZE * 3];
 
     // LPF
     private final float[] lpfPrevData = new float[3];
@@ -95,8 +97,208 @@ public class MonitorActivity extends AppCompatActivity implements SensorEventLis
         setContentView(R.layout.activity_monitor);
         setSensors();
         c = this.getBaseContext();
-        window = new DataWindow(RECORDS_SEC);
         initPlot();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.startMonitoringButton) {
+            if (!monitoringStatus) {
+                startMonitoring();
+            }
+            else {
+                /** no action performed while monitoring **/
+            }
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            if (count == 0) {rawData.append(x).append(",").append(y).append(",").append(z);}
+            else {rawData.append(",").append(x).append(",").append(y).append(",").append(z);}
+            count++;
+            // System.arraycopy(event.values, 0, rawAccData, rawAccDataIdx, 3);
+            // applyLPF();
+            // filteredData.append(",").append(lpfPrevData[0]).append(",").append(lpfPrevData[1]).append(",").append(lpfPrevData[2]);
+            if (count == SAMPLE_SIZE) {
+                writeOnFile("\n" + rawData.toString(), inferenceTempFile);
+                Log.i("Save Window"+monitoringCounter, "");
+                count = 0;
+                rawData = new StringBuilder();
+                filteredData = new StringBuilder();
+                monitoringCounter++;
+            }
+        }
+
+        if (monitoringCounter == MONITORING_REPETITIONS){
+            stopMonitoring();
+            returnInference();
+        }
+    }
+
+    private void applyLPF() {
+        final float tm = System.nanoTime();
+        final float dt = ((tm - beginTime) / 1000000000.0f) / count;
+
+        final float alpha = rc / (rc + dt);
+        // alpha = 0.0909
+
+        if (count == 0) {
+            lpfPrevData[0] = (1 - alpha) * rawAccData[0];
+            lpfPrevData[1] = (1 - alpha) * rawAccData[1];
+            lpfPrevData[2] = (1 - alpha) * rawAccData[2];
+        } else {
+            lpfPrevData[0] = alpha * lpfPrevData[0] + (1 - alpha) * rawAccData[count * 3];
+            lpfPrevData[1] = alpha * lpfPrevData[1] + (1 - alpha) * rawAccData[(count * 3) + 1];
+            lpfPrevData[2] = alpha * lpfPrevData[2] + (1 - alpha) * rawAccData[(count * 3) + 2];
+        }
+        ++count;
+    }
+
+    private void startMonitoring() {
+        Chronometer timeRecorded = findViewById(R.id.monitoringChronometer);
+        initTempFiles();
+        sm.registerListener(this, sAcceleration, SensorManager.SENSOR_DELAY_GAME); // SENSOR_DELAY_GAME: 20ms sample interval
+        monitoringStatus = true;
+        filteredData = new StringBuilder();
+        rawData = new StringBuilder();
+        timeRecorded.setBase(SystemClock.elapsedRealtime());
+        timeRecorded.start();
+        count=0;
+    }
+
+    private void stopMonitoring() {
+        Chronometer focus = findViewById(R.id.monitoringChronometer);
+        sm.unregisterListener(this, sAcceleration);
+        monitoringStatus = false;
+        focus.setBase(SystemClock.elapsedRealtime());
+        focus.stop();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) { }
+
+    public void setSensors() {
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sAcceleration = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+    }
+
+    public void initTempFiles() {
+        inferenceTempFile = new File(c.getFilesDir(), "temp_sample_data.csv");
+        Log.i("path", c.getFilesDir().toString());
+        // filteredDataFile = new File(c.getFilesDir(), "data_collection_filtered_" + this.overlapProgress + ".csv");
+        // TODO also check other files
+        if (!inferenceTempFile.exists()) {
+            Log.i("create file", c.getFilesDir().toString());
+            StringBuilder init = new StringBuilder("accx0,accy0,accz0");
+            for (int i = 1; i < SAMPLE_SIZE; i++) {
+                init.append(",accx").append(i).append(",accy").append(i).append(",accz").append(i);
+            }
+            try {
+                FileWriter fw = new FileWriter(inferenceTempFile, true);
+                fw.write(init.toString());
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void writeOnFile(String data, File file) {
+        try {
+            FileWriter fw = new FileWriter(file, true);
+            fw.write(data);
+            fw.close();
+        } catch (IOException e) {
+            Log.i("ERROR", e.toString());
+        }
+    }
+
+    private void returnInference() { /**
+     // Building a model evaluator from a PMML file
+     Evaluator evaluator = new LoadingModelEvaluatorBuilder()
+     .load(new File("model.pmml"))
+     .build();
+
+     // Perforing the self-check
+     evaluator.verify();
+
+     // Printing input (x1, x2, .., xn) fields
+     List<InputField> inputFields = evaluator.getInputFields();
+     System.out.println("Input fields: " + inputFields);
+
+     // Printing primary result (y) field(s)
+     List<TargetField> targetFields = evaluator.getTargetFields();
+     System.out.println("Target field(s): " + targetFields);
+
+     // Printing secondary result (eg. probability(y), decision(y)) fields
+     List<OutputField> outputFields = evaluator.getOutputFields();
+     System.out.println("Output fields: " + outputFields);
+     **/
+        // Iterating through columnar data (eg. a CSV file, an SQL result set)
+        //while(true){
+        // Reading a record from the data source
+
+        Map<String, ?> arguments = retrieveInferenceDataFromFile(inferenceTempFile.getAbsolutePath(), false);
+        //   if(arguments == null){
+        //       break;
+        //       }
+        //}
+
+        /**
+         // Evaluating the model
+         Map<String, ?> results = evaluator.evaluate(arguments);
+
+         // Decoupling results from the JPMML-Evaluator runtime environment
+         results = EvaluatorUtil.decodeAll(results);
+
+         // Writing a record to the data sink
+         writeRecord(results);
+         }
+
+         // Making the model evaluator eligible for garbage collection
+         evaluator = null;
+         **/
+    }
+
+    public static Map<String, String> retrieveInferenceDataFromFile(String filePath, boolean dupKeyOption) {
+        HashMap<String, String> map = new HashMap<>();
+        String line;
+        Log.i("path in map", filePath);
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while ((line = reader.readLine()) != null) {
+                String[] keyValuePair = line.split(":", 2);// TODO fix map of arguments for ML inference
+                if (keyValuePair.length > 1) {
+                    String key = keyValuePair[0];
+                    Log.i("key: ", key);
+                    String value = keyValuePair[1];
+                    Log.i("value: ", value);
+                    map.put(key, value);
+                    //if (dupKeyOption) {
+                    //    map.put(key, value);
+                    // } else {
+                    //    map.putIfAbsent(key, value);
+                    //}
+                } else {
+                    Log.i("no key:value: ","line"+line);
+                    // System.out.println("No Key:Value found in line, ignoring: " + line); // debug
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("Fail to Read file in inference map", "");
+        }
+        //File temp = new File(filePath);
+        //temp.delete(); // delete temporary File
+        Log.d("MAP OF SAMPLE", map.toString() );
+        return map;
+
     }
 
     private void initPlot() {
@@ -145,195 +347,4 @@ public class MonitorActivity extends AppCompatActivity implements SensorEventLis
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.startMonitoringButton) {
-            if (!monitoringStatus) {
-                startMonitoring();
-            }
-        } else {
-            throw new NoSuchElementException();
-        }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            rawData.append(",").append(x).append(",").append(y).append(",").append(z);
-            System.arraycopy(event.values, 0, rawAccData, 0, 3);
-            applyLPF();
-            filteredData.append(",").append(lpfPrevData[0]).append(",").append(lpfPrevData[1]).append(",").append(lpfPrevData[2]);
-            Log.i("LOG","Count: " + count + " Lines: " + filteredData.toString().split(",").length/3);
-            if (count == SAMPLE_SIZE) {
-                monitoringCounter++;
-                rawData.replace(0, 1, "");
-                filteredData.replace(0, 1, "");
-                if (monitoringCounter == MONITORING_REPETITIONS){
-                    stopMonitoring();
-                } else {
-                    startNewWindow();
-                }
-            }
-        }
-    }
-
-    private void applyLPF() {
-        final float tm = System.nanoTime();
-        final float dt = ((tm - beginTime) / 1000000000.0f) / count;
-
-        final float alpha = rc / (rc + dt);
-        // alpha = 0.0909
-
-        if (count == 0) {
-            lpfPrevData[0] = (1 - alpha) * rawAccData[0];
-            lpfPrevData[1] = (1 - alpha) * rawAccData[1];
-            lpfPrevData[2] = (1 - alpha) * rawAccData[2];
-        } else {
-            lpfPrevData[0] = alpha * lpfPrevData[0] + (1 - alpha) * rawAccData[0];
-            lpfPrevData[1] = alpha * lpfPrevData[1] + (1 - alpha) * rawAccData[1];
-            lpfPrevData[2] = alpha * lpfPrevData[2] + (1 - alpha) * rawAccData[2];
-        }
-        ++count;
-    }
-
-    private void startMonitoring() {
-        Chronometer timeRecorded = findViewById(R.id.monitoringChronometer);
-        initTempFiles();
-        sm.registerListener(this, sAcceleration, SensorManager.SENSOR_DELAY_GAME);
-        monitoringStatus = true;
-        monitoringCounter = 0;
-        count = 0;
-        filteredData = new StringBuilder();
-        rawData = new StringBuilder();
-        startNewWindow();
-        timeRecorded.setBase(SystemClock.elapsedRealtime());
-        timeRecorded.start();
-    }
-
-    private void stopMonitoring() {
-        Chronometer focus = findViewById(R.id.monitoringChronometer);
-        writeOnFile(filteredData.toString() + "\n", inferenceTempFile);
-        sm.unregisterListener(this, sAcceleration);
-        monitoringStatus = false;
-        focus.setBase(SystemClock.elapsedRealtime());
-        focus.stop();
-        // TODO compute results
-        // TODO plot results
-        // inferenceTempFile.delete();
-    }
-
-    private void returnInference() { /**
-        // Building a model evaluator from a PMML file
-        Evaluator evaluator = new LoadingModelEvaluatorBuilder()
-                .load(new File("model.pmml"))
-                .build();
-
-        // Perforing the self-check
-        evaluator.verify();
-
-        // Printing input (x1, x2, .., xn) fields
-        List<InputField> inputFields = evaluator.getInputFields();
-        System.out.println("Input fields: " + inputFields);
-
-        // Printing primary result (y) field(s)
-        List<TargetField> targetFields = evaluator.getTargetFields();
-        System.out.println("Target field(s): " + targetFields);
-
-        // Printing secondary result (eg. probability(y), decision(y)) fields
-        List<OutputField> outputFields = evaluator.getOutputFields();
-        System.out.println("Output fields: " + outputFields);
-
-        // Iterating through columnar data (eg. a CSV file, an SQL result set)
-        while(true){
-            // Reading a record from the data source
-            Map<String, ?> arguments = readRecord();
-            if(arguments == null){
-                break;
-            }
-
-            // Evaluating the model
-            Map<String, ?> results = evaluator.evaluate(arguments);
-
-            // Decoupling results from the JPMML-Evaluator runtime environment
-            results = EvaluatorUtil.decodeAll(results);
-
-            // Writing a record to the data sink
-            writeRecord(results);
-        }
-
-        // Making the model evaluator eligible for garbage collection
-        evaluator = null;
-    **/
-    }
-
-    public static Map<String, String> retrieveInferenceDataFromFile(String filePath, boolean dupKeyOption) {
-        HashMap<String, String> map = new HashMap<>();
-        String line;
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            while ((line = reader.readLine()) != null) {
-                String[] keyValuePair = line.split(":", 2);
-                if (keyValuePair.length > 1) {
-                    String key = keyValuePair[0];
-                    String value = keyValuePair[1];
-                    if (dupKeyOption) {
-                        map.put(key, value);
-                    } else {
-                        map.putIfAbsent(key, value);
-                    }
-                } else {
-                    // System.out.println("No Key:Value found in line, ignoring: " + line); // debug
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        File temp = new File(filePath);
-        temp.delete(); // delete temporary File
-        return map;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) { }
-
-    public void setSensors() {
-        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sAcceleration = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-    }
-
-    public void initTempFiles() {
-        inferenceTempFile = new File(c.getCacheDir(), "temp_sample_data.csv");
-        if (!inferenceTempFile.exists()) {
-            StringBuilder init = new StringBuilder("accx0,accy0,accz0");
-            for (int i = 1; i < SAMPLE_SIZE; i++) {
-                init.append(",accx").append(i).append(",accy").append(i).append(",accz").append(i);
-            }
-            try {
-                FileWriter fw = new FileWriter(inferenceTempFile, true);
-                fw.write(init.toString());
-                fw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void writeOnFile(String data, File file) {
-        try {
-            FileWriter fw = new FileWriter(file, true);
-            fw.write(data);
-            fw.close();
-        } catch (IOException e) {
-            Log.i("ERROR", e.toString());
-        }
-    }
-
-    public void startNewWindow() {
-        writeOnFile(filteredData.toString() + "\n", inferenceTempFile);
-        count = 0;
-        rawData = new StringBuilder();
-        filteredData = new StringBuilder();
-    }
 }
